1f50143c218951311ab588d84c15b9e25f6bb973
Fabricio-Lemos <fabriciosantana349@gmail.com>
2021-01-14 16:55:31 -0300

    Update index.js

--- Projeto_API_RESTfull/src/database/index.js
+++ Projeto_API_RESTfull/src/database/index.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize')
 const dbconfig = require('../config/database')
 const conection = new Sequelize(dbconfig)
+const User = require('../models/User')
 
-
+User.init(conection)
 
 
 module.exports = conection;
