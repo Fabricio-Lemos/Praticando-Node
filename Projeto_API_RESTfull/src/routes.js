eec517c2be7a9985e0267e3c975366d8153f5683
Fabricio-Lemos <fabriciosantana349@gmail.com>
2021-01-14 16:55:46 -0300

    Update routes.js

--- Projeto_API_RESTfull/src/routes.js
+++ Projeto_API_RESTfull/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const UserController = require('./controllers/UserControler')
 
-router.get('/', (req, res) => {
-    return res.send('Funciona')
-})
+router.get('/users', UserController.index);
+router.post('/users', UserController.store);
+router.put('/users/:user_id', UserController.update);
+router.delete('/users/:user_id', UserController.delete);
 
 module.exports = router
