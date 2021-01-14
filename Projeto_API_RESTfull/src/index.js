865eca10a468f4f1c9bded0df479c22f144f1824
Fabricio-Lemos <fabriciosantana349@gmail.com>
2021-01-14 16:55:35 -0300

    Update index.js

--- Projeto_API_RESTfull/src/index.js
+++ Projeto_API_RESTfull/src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const routes = require('./routes')
 app.use(express.json())
 app.use(routes)
+require('./database')
 
 app.listen(8081, function(){
     console.log('Servidor Iniciado!')
