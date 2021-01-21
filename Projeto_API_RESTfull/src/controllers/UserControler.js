a8659492b50c94ad507abcd92fb643e92bc05070
Fabricio-Lemos <fabriciosantana349@gmail.com>
2021-01-21 16:42:27 -0300

    login

--- Projeto_API_RESTfull/src/controllers/UserControler.js
+++ Projeto_API_RESTfull/src/controllers/UserControler.js
@@ -1,6 +1,44 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 module.exports = {
+    async login (req, res){
+        const {password, email, islogged} = req.body
+        const user = await User.findOne({where: {email}})
+
+        if (!user){
+            return res.status(400).send({
+                status: 0,
+                mensage: 'E-Mail ou Senha Incorreta'
+            })
+        }
+
+        if (!bcrypt.compareSync(password, user.password)){
+            return res.status(400).send({
+                status: 0,
+                mensage: 'E-Mail ou Senha Incorreta'
+            })
+        }
+
+        const user_id = user.id
+
+        await User.update({
+            islogged
+        }, {
+            where: {
+                id: user_id
+            }
+        })
+
+        user.password = undefined
+
+        return res.status(200).send({
+            status: 1,
+            mensage: 'Usuário, logado com sucesso!',
+            user
+        })
+    },
+
     async index(req, res) {
         const users = await User.findAll();
         
@@ -24,17 +62,23 @@ module.exports = {
     async update(req, res) {
         const {name, password, email} = req.body;
         const {user_id} = req.params;
-        await User.update({
-            name, password, email
-        }, {
-            where: {
-                id: user_id
-            }
-        })
-        return res.status(200).send({
-            status: 1,
-            mensage: 'Usuário atualizado com sucesso'
-        })
+
+          await bcrypt.genSalt(10, function(err, salt){
+                    bcrypt.hash(password, salt, function(err, hash){
+                        const password = hash
+                        User.update({
+                            name, password, email
+                        }, {
+                            where: {
+                                id: user_id
+                            }
+                        })
+                        return res.status(200).send({
+                            status: 1,
+                            mensage: 'Usuário atualizado com sucesso'
+                        })
+                    })
+            })
     },
 
     async delete(req, res) {
